860ae54fe1d7e3dd530916307a847be63d744b73
teruyo14 <113246015+teruyo14@users.noreply.github.com>
2024-10-13 01:08:30 +0900

    Update ingestion.py

--- Advanced-RAG/ingestion.py
+++ Advanced-RAG/ingestion.py
@@ -7,9 +7,7 @@ from langchain_openai import OpenAIEmbeddings
 load_dotenv()
 
 urls = [
-    "https://lilianweng.github.io/posts/2023-06-23-agent/",
-    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
-    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    # data
 ]
 
 docs = [WebBaseLoader(url).load() for url in urls]
